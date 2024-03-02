2c5430483279699c0996e404cbe4052cb154f9f9
sayujkuickel <sayujkuickel@gmail.com>
2024-03-02 11:21:54 +0545

    image fix?

--- src/constants.js
+++ src/constants.js
@@ -1,41 +1,43 @@
+import windowMaximizeIcon from "./assets/icons/window-maximize-regular.svg";
+
 export const solutions = [
   {
     id: 1,
     title: "Investment-project",
     description:
       " identification  venture funding , crowd investment business remodeling",
-    url: "/src/assets/icons/window-maximize-regular.svg",
+    url: windowMaximizeIcon,
   },
   {
     id: 2,
     title: "Finance ",
     description:
       "cost management, ratio analysis, capital budgeting, merger and acquisition, portfolio managment, dividend decision",
-    url: "/src/assets/icons/window-maximize-regular.svg",
+    url: windowMaximizeIcon,
   },
   {
     id: 3,
     title: "digital marketing",
     description: "content and digital marketing camping",
-    url: "/src/assets/icons/window-maximize-regular.svg",
+    url: windowMaximizeIcon,
   },
   {
     id: 4,
     title: "Business restructuring and redesign",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil enim perspiciatis ipsa non numquam incidunt .",
-    url: "/src/assets/icons/window-maximize-regular.svg",
+    url: windowMaximizeIcon,
   },
   {
     id: 5,
     title: "Data management and IT solution",
     description: "",
-    url: "/src/assets/icons/window-maximize-regular.svg",
+    url: windowMaximizeIcon,
   },
   {
     id: 6,
     title: "HR Management",
     description: "",
-    url: "/src/assets/icons/window-maximize-regular.svg",
+    url: windowMaximizeIcon,
   },
 ];

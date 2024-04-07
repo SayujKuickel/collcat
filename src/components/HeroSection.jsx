d1c332c0c897b099ee40143ba7890240369981c7
sayujkuickel <sayujkuickel@gmail.com>
2024-04-07 17:42:13 +0545

    CHANGE : Image Filetype

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -15,7 +15,11 @@ export default function HeroSection({ children, image, height }) {
         className={`container mx-auto  relative rounded-none md:rounded-2xl overflow-hidden 
         ${height === "full" ? "h-[40rem]" : "h-[20rem]"}`}
       >
-        <img src={image} className="w-full h-full object-cover" />
+        <img
+          src={image}
+          className="w-full h-full object-cover"
+          alt="Hero Image"
+        />
         <div className="absolute backdrop-blur-sm bg-[#ececec66] backdrop-brightness-90 text-center z-2 -top-1/2 -left-1/2 translate-x-1/2 translate-y-1/2 w-full h-full p-2 flex justify-center flex-col gap-5">
           {children}
         </div>
